fe6c2a38010d2be4cad492ef351a33f67cb64596
Lucía Ferrer Duaso <100429154@alumnos.uc3m.es>
2023-04-04 09:49:57 +0200

    10 NUM_TRIALS + prints for dimensions

--- content/adversarial/lucia/my_recover.py
+++ content/adversarial/lucia/my_recover.py
@@ -22,7 +22,8 @@ class KNNRecovery:
     def skip_next_state(self, transitions):
         dims_indexes = list(range(0, len(transitions[0]))) #-> tuple tamaño 4 [(prev_state, action, obser, rewards)]
         for _ in range(self.state_dims): dims_indexes.pop(len(dims_indexes) - 2) #indexes to remove: new dim_indexes [0,1,2,4] : no next_state
-        print('Dim_indexes{dims_indexes}')
+        print('Len(transitions[0])->',len(transitions[0]))
+        print('Dim_indexes->',dims_indexes)
         if transitions.ndim>1: 
             print('Transitions from skip_state with ndim>1->', transitions[:, dims_indexes][0])
         else:
@@ -34,6 +35,8 @@ class KNNRecovery:
         if not self.consider_next_state: transitions = self.skip_next_state(transitions)
         closest_distances, closest_idxs = self.tree.query(transitions, k=self.k)
         #TODO: Verify why different ks are used as an input, if only referenced k=0 (nearest)
+        print('Self_data->', self.data[0])
+        print('Self_data Dimension->', len(self.data[0]))
         return closest_distances[0], self.data[closest_idxs][:, :, -self.state_dims-1:-1][0]
 
     def new_state_from_parents(self, distances, parents):
--- content/config/defaults.py
+++ content/config/defaults.py
@@ -1,2 +1,2 @@
 root = 'data/ppo'
-NUM_TRIALS = 50
+NUM_TRIALS = 10
