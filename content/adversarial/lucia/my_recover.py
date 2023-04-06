398963f24d5c76ee252d7b211bd281d789b0f6f9
Lucía Ferrer Duaso <100429154@alumnos.uc3m.es>
2023-04-06 12:42:53 +0200

    Comments + skip state for .query()

--- content/adversarial/lucia/my_recover.py
+++ content/adversarial/lucia/my_recover.py
@@ -27,29 +27,25 @@ class KNNRecovery:
             Input: X -> not normalized
             Stored structures: 
             - self.data -> Normal transitions, if window we only store from n-1 Transition. 
-            - self.X -> Processed transitions, with increments, and normalized. 
+            - self.X -> Processed transitions, with wnd size transitions, increments, and normalized. 
             - self.tree -> Structure for BinarySearch. 
         """
         self.data = self.defense.train[self.window-1:] if self.window > 2 else self.defense.train
-        self.X = X if not self.diff_state and self.consider_transition and self.window<=2 else self.transform_transition(X)
+        self.X = X if not self.diff_state and self.consider_transition and self.window<=2 else self.wnd_transform_transition(X)
         print(f"Parameter for norm_param \t self.X type->{type(self.X)}, self.X shape->{self.X.shape}")
+        
         # Normalize the data and store the parameters with correct dimension
         self.norm_values = self.defense.norm_parameters(self.X)  # self.norm_translation, self.norm_scaling
         print(f"Parameter for defense.process \t  self.norm_values[0]-type->{type(self.norm_values)}, self.norm_values[0]-shape->{self.norm_values[0].shape}")
         print("Are the defense and recovery same norm_values? -> ", self.defense.norm_translation == self.norm_values[0])
         self.X = self.defense.process_transitions(self.X, self.norm_values)
 
-        if self.consider_transition: 
-            tree =  self.skip_next_state(self.X)            
-        elif not self.consider_next_state:
-            tree = self.skip_next_transition(self.X)
-        else:
-            tree = self.defense.tree 
-
+        # To build the tree if specified skip 'attacked info' -> Next_State, Reward, Action.
+        tree =  self.skip_next_state(self.X) if self.consider_transition else self.skip_next_transition(self.X) if not self.consider_next_state else self.defense.tree
         print(f"Parameter for BallTree \t tree type->{type(tree)}, tree shape->{tree.shape}")
         self.tree = BallTree(tree)
 
-    def transform_transition(self, X):
+    def wnd_transform_transition(self, X):
         """
         This method transform the current transitions to the desired format: 
             Input: np.array with shape (m,n) -> n = (state, action, next_state, reward)
@@ -59,7 +55,7 @@ class KNNRecovery:
             - Only States | State+Action+Reward
         """
         # Reward/Action, or not. -> [Sn, An, Rn]    -> (S0,A0,R0), (S1,A1,R1), (S2,A2,R2) ...
-        x = X[:,:self.state_dims] if not self.consider_transition else self.skip_next_state(X)
+        x = X[:,:self.state_dims] if not self.consider_transition else self.skip_next_state(X) if not self.consider_next_state else X
         if self.transition_dmin is None: self.transition_dmin = x.shape[1]
 
         # Increment difference or not.  -> [Sn+1 - Sn] -> ΔS1-0, ΔS2-1, ΔS3-2, ...
@@ -101,15 +97,17 @@ class KNNRecovery:
             Input: Last Wnd Raw transitions
             Output: List with K simple transitions (Sn, A, An+1, R)
         """
-        #Transform and process the transition
-        print(f"Parameter for find_parent/transform_transition  t-type->{type(transition)}, shape-t->{transition.shape}")
-        transitions = self.transform_transition(transition)
-        print(f"Parameter for process  transition-type->{type(transitions)}, transition-shape->{transitions.shape}")
-        print(f"Parameter for process  self.norm_values[0]-type->{type(self.norm_values[0])}, self.norm_values[0]-shape->{self.norm_values[0].shape}")
+
+        #Transform to obtain wnd size transitions and process the transition
+        transitions = self.wnd_transform_transition(transition)
         transitions = self.defense.process_transitions(transitions, self.norm_values) 
+
+        #Skip attacked info: State/Reward/Action' is specified
+        transitions = self.skip_next_transition(transitions) if not self.consider_transition else self.skip_next_state(transitions) if not self.consider_next_state else transitions
         
-        closest_distances, closest_idxs = self.tree.query(transitions.reshape(1,-1), k=self.k)
-        #Indexes need to be verified. 
+        #Search for K neighbours
+        closest_distances, closest_idxs = self.tree.query(transitions, k=self.k)
+
         #In self.data we have the simple transitions starting in n-1 state. (state, action, next_state, reward)
         return closest_distances[0], self.data[closest_idxs][:, :, -self.state_dims-1:-1][0]
 
