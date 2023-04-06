2b3e9bc0a7c4655763a0140408af49d1de6d1f3a
Lucía Ferrer Duaso <100429154@alumnos.uc3m.es>
2023-04-06 13:01:31 +0200

    Print

--- content/adversarial/lucia/my_recover.py
+++ content/adversarial/lucia/my_recover.py
@@ -56,7 +56,10 @@ class KNNRecovery:
         """
         # Reward/Action, or not. -> [Sn, An, Rn]    -> (S0,A0,R0), (S1,A1,R1), (S2,A2,R2) ...
         x = X[:,:self.state_dims] if not self.consider_transition else self.skip_next_state(X) if not self.consider_next_state else X
-        if self.transition_dmin is None: self.transition_dmin = x.shape[1]
+        print('X_shape->', x.shape[0], x.shape[1])
+        print('Transition_shape->', self.transition_dmin)
+        if self.transition_dmin is None: self.transition_dmin = x.shape[0]
+        print('Transition_shape->', self.transition_dmin)
 
         # Increment difference or not.  -> [Sn+1 - Sn] -> ΔS1-0, ΔS2-1, ΔS3-2, ...
         if self.diff_state: x = np.diff(x) 
