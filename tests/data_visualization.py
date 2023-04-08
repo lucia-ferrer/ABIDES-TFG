d64a8b4256adc392fdd48359acc62302019e604c
Lucía Ferrer Duaso <100429154@alumnos.uc3m.es>
2023-04-08 18:01:44 +0200

    Tests for series time and distributions of the data

--- tests/data_visualization.py
+++ tests/data_visualization.py
@@ -7,15 +7,15 @@ if __name__ == '__main__':
     #transitions = {id: pd.read_csv(f'data/transitions_{id}.csv', header=0).values for id in ids}
     transitions = {id: pd.read_csv(f'transitions_{id}.csv', header=0).values for id in ids}
     state_dimns = {'MM': 28, 'PT1':21}
-    for id in ids: 
-        transitions[id]['ITER'] = transitions[id].index
-        transitions[id]['Date'] = (pd.date_range(start=pd.datetime.datetime(2023, 3, 1), 
-                                     periods=transitions[id].shape[0], 
+    for id, data in transitions.items: 
+        data['Date'] = (pd.date_range(start=pd.datetime.datetime(2023, 3, 1), 
+                                     periods=data.shape[0], 
                                      freq='30S'))
+        data.set_index('Date')
 
-        print(f'Shape of data {transitions[id].shape}')
+        print(f'Shape of data for {id} -> {transitions[id].shape}')
         for i in range(state_dimns[id]):
-            transitions[id][i].plot(figsize=(12,5))
+            data[i].plot(figsize=(12,5))
 
 
 
