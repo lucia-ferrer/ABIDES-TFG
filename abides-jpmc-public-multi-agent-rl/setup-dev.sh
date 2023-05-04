5fa730fe520fe010e32395eb0ede002d339aeb72
lucia-ferrer <100429154@alumnos.uc3m.es>
2023-05-04 12:08:02 +0200

    gym

---
+++ abides-jpmc-public-multi-agent-rl/setup-dev.sh
@@ -0,0 +1,8 @@
+python -m pip install -r requirements.txt
+python -m pip install -r requirements-dev.txt
+cd abides-core
+python setup.py develop
+cd ../abides-markets
+python setup.py develop
+cd ../abides-gym
+python setup.py develop
